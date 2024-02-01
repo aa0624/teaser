210b3ea4d932e97d52e11b8db299115ebfae4cc2
aa0624 <24aaggarwal@athenian.org>
2024-01-31 23:08:26 -0800

    test

--- js.js
+++ js.js
@@ -1,35 +1,68 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  
+window.onload = function () {
+    setCount();
+  };
+
+  
 let darkMode = false;
-const toggle = document.querySelector("#toggle");
+const toggle = document.querySelector("#toggle")
 toggle.addEventListener("click", () => {
   if (darkMode === false) {
+    document.getElementById("toggle").innerHTML = "lmao";
     toggle.classList.add("darkMode");
     darkMode = true;
   } else {
+    document.getElementById("toggle").innerHTML = "Toggle Dark Mode";
     toggle.classList.remove("darkMode");
     darkMode = false;
+   
   }
+  
 });
 
-try{
-  count = parseInt(window.localStorage.getItem("countStorage"))
-}
-catch{
-  count = 0;
-  window.lcalStorage.setItem("countStorage", count)
-  console.log("empty")
-}
-
-function add() {
-   document.getElementById("count").innerHTML = parseInt(document.getElementById("count").innerHTML) +1;
-  count=count+1;
-  if (count>20){
-  alert("you have 20!")
-}
-}
-
-function buy(price){
-  if(count>price){
-    count = count-price;
+function setCount(){
+  let count;
+  let windowCount = window.localStorage.getItem("countStorage");
+  if (windowCount==="null"){
+    count = 0;
+    window.localStorage.setItem("countStorage", count);
+    console.log("countStorage empty");
   }
+  else{
+    count = windowCount;
+    console.log("countStorage retrieved");
+  }
+  console.log(count);
+  document.getElementById("countID").innerHTML = count;
 }
 
+
+ 
+document.querySelector("#addButton").addEventListener("click", () => {
+  add();
+});
+  
+let alert20 = false;
+function add() {
+  let count = window.localStorage.getItem("countStorage");
+  count = parseInt(count) + 1;
+  window.localStorage.setItem("countStorage", count);
+  document.getElementById("countID").innerHTML = count;
+  if (parseInt(count)>20){
+    if (alert20 === false){
+      alert("you have 20! you can now buy an autoClicker");
+      alert20 = true;
+      document.getElementByID("autoClicker").style.visibility = "visible";
+    }
+}
+}
+
+
+document.querySelector("#autoClicker").addEventListener("click", () => {
+  
+});
+  
+  
+  
+});
